074757fa8d2e73c285a4b576403408972a2f8934
Jon Seymour <jon.seymour@gmail.com>
2014-10-17 16:19:38 +1100

    Rename sphere-serial -> sphere-go-serial so that default bin matches.

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,4 +34,4 @@ cd .gopath/src/github.com/${OWNER}/${PROJECT_NAME}
 go get -d -v ./...
 
 # build each of the tools and put them in the bin folder for packaging.
-go build -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" -o ./bin/sphere-go-serial -x github.com/ninjablocks/sphere-utils/tools/sphere-serial
+go build -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" -o ./bin/sphere-go-serial -x github.com/ninjablocks/sphere-utils/tools/sphere-go-serial
