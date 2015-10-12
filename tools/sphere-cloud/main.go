a230fd4d36f702c0a57db755a04423db7dd68d6d
Jon Seymour <jon@wildducktheories.com>
2015-10-12 19:33:21 +1100

    Add support for setting and getting site tags.

--- tools/sphere-cloud/main.go
+++ tools/sphere-cloud/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ninjasphere/go-ninja/cloud"
+	"io/ioutil"
 	"os"
 )
 
@@ -10,7 +12,10 @@ const (
 	USAGE = "sphere-cloud\n" +
 		"   register email password name\n" +
 		"   authenticate email password\n" +
-		"   activtate token node\n"
+		"   activtate token node\n" +
+		"   get-tag token site tag\n" +
+		"   post-tag token site tag < body\n" +
+		"   put-tag token site tag < body\n"
 )
 
 func main() {
@@ -50,6 +55,40 @@ func main() {
 		if err := cloud.CloudAPI().ActivateSphere(args[1], args[2]); err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
+		}
+	case "get-tag":
+		if len(args) < 4 {
+			fmt.Fprintf(os.Stderr, USAGE)
+			os.Exit(1)
+		}
+		var message json.RawMessage
+		if err := cloud.CloudAPI().GetTag(args[1], args[2], args[3], &message); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		} else {
+			if _, err := os.Stdout.Write(message); err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				os.Exit(1)
+			}
+		}
+	case "post-tag", "put-tag":
+		if len(args) < 4 {
+			fmt.Fprintf(os.Stderr, USAGE)
+			os.Exit(1)
+		}
+		if buffer, err := ioutil.ReadAll(os.Stdin); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		} else {
+			data := map[string]interface{}{}
+			if err := json.Unmarshal(buffer, &data); err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				os.Exit(1)
+			}
+			if err := cloud.CloudAPI().SetTag(args[1], args[2], args[3], data, args[0] == "put-tag"); err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				os.Exit(1)
+			}
 		}
 	default:
 		fmt.Fprintf(os.Stderr, "unrecognized argument: %s\n", args[0])
